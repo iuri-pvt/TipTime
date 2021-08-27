7df3cae82f69e9ba401a4f0bd14a7ae48ae96d49
dasilvaiuri <iuridasilva@gmail.com>
2021-08-27 17:40:45 -0300

    inicial

---
+++ app/src/main/java/br/com/iuridasilva/tiptime/MainActivity.kt
@@ -0,0 +1,57 @@
+package br.com.iuridasilva.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.iuridasilva.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    //Definir binding com hierarquia ao invés de usar findByID
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+        //recuperar o texto do editable e converter em double
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        //binding com os radioButtons
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        val roundUp = binding.roundUpSwitch.isChecked
+        //se checado, arredonda para cima com "ceil"
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+    }
+
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
